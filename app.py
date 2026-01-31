f4882a3662e99119de079f378b634daae490ca84
Liyutsa Zirange <Liyutsa@gmail.com>
2026-01-31 09:05:46 +0000

    Imported

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, render_template
 import joblib
 import pandas as pd
 from datetime import datetime
